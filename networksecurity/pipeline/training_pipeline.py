f52827aa36d16aea909e8afa536608948344c906
HumourSpeech <mishranitin6076@gmail.com>
2026-01-19 12:13:44 +0530

    Added Validation and Transformation

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -31,3 +31,26 @@ class TrainingPipeline:
             return data_ingestion_artifact
         except Exception as e:
             raise NetworkSecurityException(e, sys)
+        
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact):
+        try:
+            data_validation_config = DataValidationConfig(training_pipeline_config=self.training_pipeline_config)
+            data_validation = DataValidation(data_validation_config=data_validation_config,
+                                             data_ingestion_artifact=data_ingestion_artifact)
+            logging.info(f"{'>>'*20} Starting Data Validation {'<<'*20}")
+            data_validation_artifact: DataValidationArtifact = data_validation.initiate_data_validation()
+            return data_validation_artifact
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+    
+    def start_data_transformation(self, data_validation_artifact: DataValidationArtifact):
+        try:
+            data_transformation_config = DataTransformationConfig(training_pipeline_config=self.training_pipeline_config)
+            data_transformation = DataTransformation(data_transformation_config=data_transformation_config,
+                                                     data_validation_artifact=data_validation_artifact)
+            logging.info(f"{'>>'*20} Starting Data Transformation {'<<'*20}")
+            data_transformation_artifact: DataTransformationArtifact = data_transformation.initiate_data_transformation()
+            return data_transformation_artifact
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+            
